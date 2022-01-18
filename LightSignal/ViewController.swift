b32cec9f4eff3a07fb0c648b42669ff34bf376a5
Vladimir Usov <vlumailbox@gmail.com>
2022-01-19 00:58:08 +0300

    Complete Light Signal

--- LightSignal/ViewController.swift
+++ LightSignal/ViewController.swift
@@ -8,12 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redView: UIView!
+    
+    @IBOutlet weak var yellowView: UIView!
+    
+    @IBOutlet weak var greenView: UIView!
+    
+    @IBOutlet weak var pressButtonText: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = 50
+        redView.alpha = 0.3
+        
+        yellowView.layer.cornerRadius = 50
+        yellowView.alpha = 0.3
+        
+        greenView.layer.cornerRadius = 50
+        greenView.alpha = 0.3
+        
     }
-
-
+    
+    var light = 1 
+    
+    @IBAction func pressButton() {
+        pressButtonText.setTitle("NEXT", for: .normal)
+        
+        switch light {
+        case 1:
+            redView.alpha = 1.0
+            yellowView.alpha = 0.3
+            greenView.alpha = 0.3
+            light = 2
+            
+        case 2:
+            redView.alpha = 0.3
+            yellowView.alpha = 1.0
+            greenView.alpha = 0.3
+            light = 3
+            
+        case 3:
+            redView.alpha = 0.3
+            yellowView.alpha = 0.3
+            greenView.alpha = 1.0
+            light = 1
+            
+        default:
+            redView.alpha = 0.3
+            yellowView.alpha = 0.3
+            greenView.alpha = 0.3
+            light = 0
+        }
+    }
+    
 }
-
